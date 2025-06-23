52903a97d2a21f62902069fa4fd5b5e97a7bc300
Yohannes <ymogos2@gmail.com>
2025-06-23 11:11:50 +0100

    removing raws in speech column if less than 1000 words

--- PartTwo.py
+++ PartTwo.py
@@ -1,12 +1,12 @@
 import pandas as pd
-
+# a-i)
 # Read the CSV file from the 'texts' directory
 df = pd.read_csv("p2-texts\p2-texts\hansard40000.csv")
 
 # Replace 'Labour (Co-op)' with 'Labour' in the 'party' column
 df['party'] = df['party'].replace('Labour (Co-op)', 'Labour')
 
-
+# a-ii)
 # Remove rows where 'party' is 'Speaker'
 df = df[df['party'] != 'Speaker']
 
@@ -19,3 +19,15 @@ df = df[df['party'].isin(top_parties)]
 #  check result
 # print(df['party'].value_counts())
 # print(top_parties)
+
+# a-iii)
+df = df[df['speech_class'] == 'Speech']
+
+# a-iv)
+# Remove rows where 'speech' text is shorter than 1000 characters
+df = df[df['speech'].str.len() >= 1000]
+
+# Print the dimensions: (rows, columns)
+print(df.shape)
+
+
