acae1faa588c94ff1edd137b0b966ddc3cce8191
Yohannes <ymogos2@gmail.com>
2025-06-23 11:01:45 +0100

    Removing the row if it is speaker-party

--- PartTwo.py
+++ PartTwo.py
@@ -6,5 +6,16 @@ df = pd.read_csv("p2-texts\p2-texts\hansard40000.csv")
 # Replace 'Labour (Co-op)' with 'Labour' in the 'party' column
 df['party'] = df['party'].replace('Labour (Co-op)', 'Labour')
 
-# Display to confirm change
-print(df['party'].value_counts())  # Optional: check if change occurred
+
+# Remove rows where 'party' is 'Speaker'
+df = df[df['party'] != 'Speaker']
+
+# Find the 4 most common party names (excluding 'Speaker')
+top_parties = df['party'].value_counts().nlargest(4).index
+
+# Keep only rows where 'party' is one of the top 4
+df = df[df['party'].isin(top_parties)]
+
+#  check result
+# print(df['party'].value_counts())
+# print(top_parties)
